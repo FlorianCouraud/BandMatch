ed34b807442bf85405dc758b41a2235f77a6e680
Wilfried Bessovi <wbsvnet@gmail.com>
2019-06-10 22:37:41 +0200

    implementing search for instrument

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @instrument = Instrument.all
+    @instruments = Instrument.all
+    @users = User.all
 
   end
 end
