67107af1433299cf68fdb43e5c6ad50fc422833c
Mathilde Couraud <mcouraud@outlook.fr>
2019-06-11 14:36:17 +0200

    CSS général footers bien en bas et seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ puts "Creating users ..."
 user1 = User.new(first_name: "Wilfried", last_name: "Bessovi", age: "30", address: "64 avenue du Prado, Marseille", city: "Marseille", email: "wbsvnet@gmail.com", password: "123456", link1: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/543406167&color=%23ff5500&inverse=false&auto_play=false&show_user=true")
 user1.remote_photo_url = "https://images.pexels.com/photos/2232981/pexels-photo-2232981.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=1&w=500"
 user1.save!
-user2 = User.new(first_name: "Jessica", last_name: "F.", age: "25", address: "49 rue de la Republique, Marseille", city: "Marseille", email: "jess.fiedler83860@gmail.com", password: "123456", link1: "https://soundcloud.com/m7mdabdelgelil/the-rains-of-castamere-game-of-thrones-solo-acoustic-guitar-ernesto-schnack")
+user2 = User.new(first_name: "Jessica", last_name: "F.", age: "25", address: "49 rue de la Republique, Marseille", city: "Marseille", email: "jess.fiedler83860@gmail.com", password: "123456", link1:
 user2.remote_photo_url = "https://images.unsplash.com/photo-1520998590883-430e4faee99e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
 user2.save!
 user3 = User.new(first_name: "Florian", last_name: "Couraud", age: "27", address: "12 rue de Rome, Marseille", city: "Marseille", email: "f.couraud13@gmail.com", password: "123456", link1: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/508558983&color=%23ff5500&inverse=false&auto_play=false&show_user=true")
@@ -39,10 +39,10 @@ user6.save!
 user7 = User.new(first_name: "Théo", last_name: "Ensinas", age: "21", address: "64 rue de Rome, Marseille", city: "Marseille", email: "kcobain@gmail.com", password: "123456", link1: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/294076069&color=%23ff5500&inverse=false&auto_play=false&show_user=true")
 user7.remote_photo_url = "https://images.unsplash.com/photo-1534385842125-8c9ad0bd123c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
 user7.save!
-user8 = User.new(first_name: "Romain", last_name: "Trickster", age: "22", address: "5 rue de la Republique, Marseille", city: "Marseille", email: "tricksters@gmail.com", password: "123456", link1: "https://soundcloud.com/louis-webb-574591449/studies-for-solo-electric-guitar-iii-death-metal")
+user8 = User.new(first_name: "Romain", last_name: "Trickster", age: "22", address: "5 rue de la Republique, Marseille", city: "Marseille", email: "tricksters@gmail.com", password: "123456", link1:
 user8.remote_photo_url = "https://image.noelshack.com/fichiers/2019/24/1/1560155136-bm-romain.jpg"
 user8.save!
-user9 = User.new(first_name: "Raph", last_name: "ACOD", age: "31", address: "142 Boulevard de la République, Marseille", city: "Marseille", email: "rclement@gmail.com", password: "123456", link1: "https://soundcloud.com/thedrummer145/solo-de-batterie-sur-tom-uniquement")
+user9 = User.new(first_name: "Raph", last_name: "ACOD", age: "31", address: "142 rue de la République, Marseille", city: "Marseille", email: "rclement@gmail.com", password: "123456", link1:
 user9.remote_photo_url = "https://image.noelshack.com/fichiers/2019/24/1/1560155137-bm-raphcarre.jpg"
 user9.save!
 user10 = User.new(first_name: "Rose", last_name: "Cobain", age: "27", address: "164 rue paradis, Marseille", city:"Marseille", email: "Rose-dc@gmail.com", password: "123456", link1: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/229064894&color=%23ff5500&inverse=false&auto_play=false&show_user=true")
@@ -110,9 +110,6 @@ trumpet = Instrument.create(name: "Trumpet")
 trombone = Instrument.create(name: "Trombone")
 violin = Instrument.create(name: "Violin")
 sing = Instrument.create(name: "Sing")
-clear_voice = Instrument.create(name: "Clear Voice")
-black_voice = Instrument.create(name: "Black Voice")
-death_voice = Instrument.create(name: "Death Voice")
 beatbox = Instrument.create(name: "Beat Box")
 ukulele = Instrument.create(name: "Ukulele")
 djembe = Instrument.create(name: "Djembe")
@@ -136,9 +133,6 @@ electro = Style.create!(name: "Electro")
 indie = Style.create!(name: "Indie Rock")
 pop = Style.create!(name: "Pop")
 metal = Style.create!(name: "Metal")
-blackmetal = Style.create!(name: "Black Metal")
-deathmetal = Style.create!(name: "Death Metal")
-neometal = Style.create!(name: "Neo Metal")
 folk = Style.create!(name: "Folk")
 classic = Style.create!(name: "Classic")
 puts "Finished style creation"
@@ -150,7 +144,6 @@ UserStyle.create!(user_id: user1.id, style_id: country.id)
 UserStyle.create!(user_id: user2.id, style_id: rock.id)
 UserStyle.create!(user_id: user2.id, style_id: pop.id)
 UserStyle.create!(user_id: user2.id, style_id: metal.id)
-UserStyle.create!(user_id: user2.id, style_id: deathmetal.id)
 UserStyle.create!(user_id: user3.id, style_id: blues.id)
 UserStyle.create!(user_id: user3.id, style_id: pop.id)
 UserStyle.create!(user_id: user3.id, style_id: techno.id)
@@ -162,12 +155,8 @@ UserStyle.create!(user_id: user6.id, style_id: electro.id)
 UserStyle.create!(user_id: user6.id, style_id: electro.id)
 UserStyle.create!(user_id: user7.id, style_id: electro.id)
 UserStyle.create!(user_id: user7.id, style_id: electro.id)
-UserStyle.create!(user_id: user8.id, style_id: blackmetal.id)
-UserStyle.create!(user_id: user8.id, style_id: deathmetal.id)
 UserStyle.create!(user_id: user8.id, style_id: metal.id)
 UserStyle.create!(user_id: user9.id, style_id: metal.id)
-UserStyle.create!(user_id: user9.id, style_id: deathmetal.id)
-UserStyle.create!(user_id: user9.id, style_id: blackmetal.id)
 UserStyle.create!(user_id: user9.id, style_id: blues.id)
 UserStyle.create!(user_id: user10.id, style_id: classic.id)
 UserStyle.create!(user_id: user10.id, style_id: pop.id)
@@ -197,8 +186,6 @@ UserInstrument.create!(user_id: user7.id, instrument_id: guitar.id, years_of_exp
 UserInstrument.create!(user_id: user7.id, instrument_id: ukulele.id, years_of_experience: "5")
 UserInstrument.create!(user_id: user8.id, instrument_id: guitar.id, years_of_experience: "8")
 UserInstrument.create!(user_id: user8.id, instrument_id: bass_guitar.id, years_of_experience: "6")
-UserInstrument.create!(user_id: user8.id, instrument_id: death_voice.id, years_of_experience: "3")
-UserInstrument.create!(user_id: user8.id, instrument_id: clear_voice.id, years_of_experience: "4")
 UserInstrument.create!(user_id: user9.id, instrument_id: drums.id, years_of_experience: "12")
 UserInstrument.create!(user_id: user10.id, instrument_id: piano.id, years_of_experience: "7")
 UserInstrument.create!(user_id: user11.id, instrument_id: guitar.id, years_of_experience: "2")
@@ -212,8 +199,5 @@ UserInstrument.create!(user_id: user13.id, instrument_id: synthesizer.id, years_
 
 
 puts "Finished association between user and instrument"
-
-
-
 
 puts "All good, ready to go!!"
